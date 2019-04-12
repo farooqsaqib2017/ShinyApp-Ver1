8b716f8cd1de93c34f87586108165edb63536a83
Farooq Saqib <33466411+farooqsaqib2017@users.noreply.github.com>
2019-04-12 16:31:13 +0100

    Update ui.R

--- ver1/ui.R
+++ ver1/ui.R
@@ -1,1 +1,50 @@
 
+
+# Define UI for data upload app ----
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel(" Fake News Detector"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      textInput(inputId = "text", label = "Text input"
+                
+      ),
+      # Input: Select a file ----
+      fileInput("file1", "File input",
+                multiple = FALSE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")),
+      
+      
+      # Input: Checkbox if file has header ----
+      
+      
+      # Input: Select separator ----
+      
+      
+      # Input: Select quotes ----
+      submitButton(text = "Submit", icon = NULL, width = NULL),
+      
+      # Horizontal line ----
+      tags$hr()
+      
+      
+    ),   # slidebar panel closed
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Data file ----
+      tableOutput("contents")
+      
+    ) # main panel closed
+    
+  ) # slidebar layout closed
+) # fluid page closed
